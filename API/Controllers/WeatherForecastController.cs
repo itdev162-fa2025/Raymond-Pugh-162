e144276e13d0aead7ee554751acc8a9117a05044
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-11-03 15:24:20 -0600

    done

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,10 +1,10 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers;
 
 [ApiController]
-
 [Route("[controller]")]
 
 public class WeatherForecastController: ControllerBase{
@@ -16,9 +16,13 @@ private static readonly string[] Summaries = new[]
 
 private readonly ILogger<WeatherForecastController> _logger;
 
-public WeatherForecastController (ILogger<WeatherForecastController> logger)
+private readonly DataContext _context;
+
+public WeatherForecastController (ILogger<WeatherForecastController> logger, 
+DataContext context)
 {
 _logger = logger;
+_context = context;
 }
 
 [HttpGet (Name = "GetWeatherForecast")]
@@ -32,6 +36,26 @@ TemperatureC = Random. Shared.Next (-20, 55),
 Summary = Summaries [Random. Shared.Next (Summaries.Length)]
 })
 .ToArray();
+}
+
+[HttpPost]
+public ActionResult<WeatherForecast> Create()
+{
+    Console.WriteLine($"Database path: {_context.DbPath}");
+    var forecast = new WeatherForecast()
+    {
+        Date = new DateOnly(),
+        TemperatureC = 75,
+        Summary = "Warm"
+    };
+    _context.WeatherForecasts.Add(forecast);
+    var success = _context.SaveChanges()>0;
+
+    if (success){
+
+        return forecast;
+    }
+    throw new Exception("Error creating WeatherForecast");
 }
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
