63dde112c3a43d62e04517cf22b214fe64f5eed6
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-12-05 18:40:52 -0600

    first commit

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -14,11 +14,43 @@ namespace API.Controllers{
         {
             this._context=context;
         }
-
+//get all
         [HttpGet(Name ="GetPosts")]
         public ActionResult<List<Post>> Get()
         {
             return this._context.Posts.ToList();
+        }
+
+        [HttpGet("{id}",Name ="GetById")]
+//get by id
+        public ActionResult<Post>GetById(Guid id){
+            var post = this._context.Posts.Find(id);
+            if(post is null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        //post
+        [HttpPost(Name ="Create")]
+
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post{
+                Id=request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date=request.Date
+            };
+
+            _context.Posts.Add(post);
+            var success=_context.SaveChanges()>0;
+
+            if (success){
+                return Ok(post);
+            }
+            throw new Exception("Error creating post");
         }
     }
 }
