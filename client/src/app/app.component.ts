d12e9cc1b2e7fd4486eae3fd86274f9336f61738
Raymond Pugh <raymondpugh@Raymonds-MacBook-Pro.local>
2024-12-06 16:06:55 -0600

    done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,27 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent implements OnInit {
-  title = 'client';
-  posts:any;
+  title = 'My Blog';
+  posts: any = []; 
 
-  constructor(private http:HttpClient){
+  constructor(private http: HttpClient) {}
 
-  }
   ngOnInit(): void {
     this.http.get('http://localhost:5022/api/posts').subscribe({
-      next:(response)=>this.posts=response,
-      error:(e)=>console.error(e),
-      complete:()=>console.log('complete')
-    })
-    //throw new Error("Method not implemented.");
+      next: (response) => (this.posts = response),
+      error: (e) => console.error('Error fetching posts:', e),
+      complete: () => console.log('Posts fetched successfully')
+    });
   }
 }
